abb06c9c3d580c47394a5a3da95e2db75321a9f9
gundarsv <267009@viauc.dk>
2020-10-20 11:43:36 +0200

    Add and remove airport implementation

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,10 +1,14 @@
-import { Container, makeStyles } from "@material-ui/core";
-import { Console, error } from "console";
+import { Container, Drawer, LinearProgress, makeStyles } from "@material-ui/core";
+
+import { AddAirport } from "components/addAirport";
+import AddIcon from "@material-ui/icons/Add";
+import { Airport } from "src/types/Airport";
+import AirportService from "src/services/airport.service";
+import DeleteIcon from "@material-ui/icons/Delete";
 import MaterialTable from "material-table";
 import React from "react";
+import { useSnackbar } from "notistack";
 import { withRouter } from "react-router-dom";
-import AirportService from "src/services/airport.service";
-import { Airport } from "src/types/Airport";
 
 const useStyles = makeStyles({
 	container: {
@@ -14,32 +18,87 @@ const useStyles = makeStyles({
 
 const Main: React.FC = () => {
 	const [airports, setAirports] = React.useState<Array<Airport>>([]);
+	const [isAddAirportOpen, setIsAddAirportOpen] = React.useState(false);
+	const [isLoading, setIsLoading] = React.useState(true);
+
 	const classes = useStyles();
+	const snackbar = useSnackbar();
 
 	React.useEffect(() => {
 		AirportService.getAirports().then(
 			response => {
-				console.log(response.data);
 				setAirports(response.data);
+				setIsLoading(false);
 			},
 			error => {
-				console.log(error);
+				snackbar.enqueueSnackbar(error.response.data[0], { variant: "error", autoHideDuration: 2000 });
+				setIsLoading(false);
 			},
 		);
-	}, [setAirports]);
+	}, [setAirports, setIsLoading, snackbar]);
+
+	const onAirportAdded = (airport: Airport) => {
+		setIsAddAirportOpen(false);
+		setIsLoading(true);
+		setAirports([...airports, airport]);
+		setIsLoading(false);
+	};
+
+	const handleRemoveAirport = (airport: Airport) => {
+		AirportService.deleteAirport(airport.id).then(
+			response => {
+				if (response.status === 200) {
+					setAirports(airports.filter(a => a.id !== airport.id));
+					snackbar.enqueueSnackbar("Airport - " + airport.iata + " was removed", { variant: "success", autoHideDuration: 2000 });
+				}
+			},
+			error => {
+				snackbar.enqueueSnackbar(error.response.data[0], { variant: "error", autoHideDuration: 2000 });
+			},
+		);
+	};
+
+	if (isLoading) {
+		return <LinearProgress />;
+	}
 
 	return (
-		<Container className={classes.container} maxWidth="xl">
-			<MaterialTable
-				title=""
-				columns={[
-					{ title: "Id", field: "id" },
-					{ title: "Iata", field: "iata" },
-					{ title: "Airport Name", field: "name" },
-				]}
-				data={airports}
-			/>
-		</Container>
+		<div>
+			<Container className={classes.container} maxWidth="xl">
+				<MaterialTable
+					style={{ fontFamily: "'Roboto', 'Helvetica', 'Arial', 'sans-serif'" }}
+					columns={[
+						{ title: "Id", field: "id" },
+						{ title: "Iata", field: "iata" },
+						{ title: "Airport Name", field: "name" },
+					]}
+					options={{ showTitle: false, sorting: true, paging: false, minBodyHeight: 750, maxBodyHeight: 750, actionsColumnIndex: -1 }}
+					data={airports}
+					actions={[
+						{
+							// eslint-disable-next-line react/display-name
+							icon: () => <DeleteIcon />,
+							tooltip: "Remove Airport",
+							onClick: (event, rowData) => {
+								handleRemoveAirport(rowData as Airport);
+							},
+						},
+						{
+							// eslint-disable-next-line react/display-name
+							icon: () => <AddIcon />,
+							tooltip: "Add Airport",
+							isFreeAction: true,
+							onClick: () => {
+								setIsAddAirportOpen(true);
+							},
+						},
+					]}
+				/>
+			</Container>
+			<Drawer anchor="right" open={isAddAirportOpen} onClose={() => setIsAddAirportOpen(false)}>
+				<AddAirport onAirportAdded={onAirportAdded} />
+			</Drawer>
+		</div>
 	);
 };
 
--- src/services/airport.service.ts
+++ src/services/airport.service.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
+import { Airport } from "src/types/Airport";
 import AuthService from "./auth.service";
 import authHeader from "./auth-header";
-import { Airport } from "src/types/Airport";
+import axios from "axios";
 
 const API_URL = "https://flyit.azurewebsites.net/api/Airport/";
 
@@ -26,6 +26,14 @@ class AirportService {
 
 	getAirports() {
 		return this.axiosInstance.get<Array<Airport>>(API_URL, { headers: authHeader() });
+	}
+
+	deleteAirport(airportId: number) {
+		return this.axiosInstance.delete<Airport>(API_URL + airportId, { headers: authHeader() });
+	}
+
+	addAirport(iata: string, airportName: string) {
+		return this.axiosInstance.post<Airport>(API_URL, { iata: iata, name: airportName }, { headers: authHeader() });
 	}
 }
 
